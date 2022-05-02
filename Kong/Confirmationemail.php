376d741b2d805408a92bc0fcf7231a1f1f4cb0bd
Kongchekfung <101616128+Kongchekfung@users.noreply.github.com>
2022-05-02 22:13:03 +0800

    Add files via upload

---
+++ Kong/Confirmationemail.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--Decription: Confirmation Email-->
+<!--Author: Kong Chek Fung-->
+<!--Date: 2 May 2022-->
+<!--Validated: OK 2 May 2022-->   
+
+<head>
+    <title>Confirmation Email</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=ul, initial-scale=1.0" />
+    <meta charset="author" content="Kong Chek Fung">
+    <meta charset="desciption" content="Confirmation Email">
+    <meta charset="keyword" content="This is the confirmation email for Online Catering System">
+    <link rel="stylesheet" href="stylekong.css"/> 
+    <title>Confirmation Email</title>
+</head>
---
+++ Kong/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=ul, initial-scale=1.0" />
+    <meta name="author" content="Kong Chek Fung" />
+    <meta name="keywords" content="Home" />
+    <meta name="description" content="Home" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <title>Home</title>
+</head>
+
+<body>
+    <?php include 'navigation.php';?>
+    <?php
+        if (isset($_SESSION['adminid'])){
+            header("location: admin.php");
+            exit();
+        }
+    ?>
+    
+</body>
+
+</html>
