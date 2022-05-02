b8b9b588dc72faa2dd5037e1f79d62cde2796947
cookieflakes2 <101045565+cookieflakes2@users.noreply.github.com>
2022-05-02 14:25:17 +0300

    commencement on 'search function'

---
+++ allan/search-button/search.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=ul, initial-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Search</title>
+    </head>
+
+    <body>
+        
+        <?php
+        /*
+            "----" is the database name where the menu tables are.
+            Create a connection to link the database to our search form
+            Create a variable that allows the system to select all values in the table '-----'
+            After creating the variable, make a query and a fetch function to fetch the data selected and display it.
+            Now, again back to search
+            with the connection already created, create another variable to select values in the table that are related to user input
+            Display it in a different page
+            That's it!
+        */
+        /*
+            $server = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "orderdb";
+            
+            //creating connection
+            $con = mysqli_connect($server, $username, $password, $dbname);
+
+            $sql = "SELECT * FROM search WHERE ".$_POST['search-input']." LIKE 's_title' ";
+        */
+        ?>
+    </body>
+</html>
