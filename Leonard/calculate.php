052ce921c1166cbe531228012a1b1b5b4426f622
LeonardLuLL <101616088+LeonardLuLL@users.noreply.github.com>
2022-05-03 16:30:45 +0800

    Delete calculate.php

--- Leonard/calculate.php
+++
@@ -1,7 +0,0 @@
-<?php
-$datetime = new DateTime; // current time = server time
-$otherTZ  = new DateTimeZone('ASIA');
-$datetime->setTimezone($otherTZ); // calculates with new TZ now
-
-echo $datetime->format('d-m-Y H:i:s T');
-?>
